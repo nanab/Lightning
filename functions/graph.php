fa9794193d2618fdb6d658862be4ac4f7b3e4498
Ola Eriksson <nanab666@gmail.com>
2013-03-22 21:16:19 +0100

    Revert "Update to work on apple"
    This reverts commit 80d87280cf2d48f521f88c35568b245eb52eec3f.

--- functions/graph.php
+++ functions/graph.php
@@ -2,18 +2,14 @@
 <?php
 //Load graph image
 include(dirname(__FILE__)."/../settings/load_settings.php");
-//Get id off device
+//DSId
 @$DSId = $_GET["DSId"];
-//Get width size of image to display
+//DSSizeX
 @$DSSizeX = $_GET["DSSizeX"];
-//Get Height size of imgage to display
+//DSSizeY
 @$DSSizeY = $_GET["DSSizeY"];
-//Get minutes to diplay on graph
+//DSMinute
 @$DSMinute = $_GET["DSMinute"];
-//Path where to save image you get from switchking temporarly
-$img = dirname(__FILE__)."/../settings/tempfiles/tempgraph.png";
-//String to get image from switchking
-$url = "http://$User:$Pass@$Ip:$Port/$FuncDS/$DSId/graph.png?width=$DSSizeX&height=$DSSizeY&minutesofhistory=$DSMinute";
-file_put_contents($img, file_get_contents($url)); //Save image from switch king to the temporarly location
+$test = "http://$User:$Pass@$Ip:$Port/$FuncDS/$DSId/graph.png?width=$DSSizeX&height=$DSSizeY&minutesofhistory=$DSMinute";
 ?>
-<img src="../settings/tempfiles/tempgraph.png" /> <!-- Display image. -->
+<img src="<?php echo $test; ?>" />
